46351f13f16cac0e04acd31156bf80ca5127fed3
KhangVo711 <vhmkhang2101101@student.ctuet.edu.vn>
2024-09-09 22:19:41 +0700

    fixDetail

--- client/src/components/home/ContentDetail.jsx
+++ client/src/components/home/ContentDetail.jsx
@@ -5,8 +5,10 @@ export default function ContentDetail(){
 
     return(
         <>
-        <div className="relative h-screen flex items-center">
-            <img className={`absolute left-[-33rem] object-cover w-full h-3/4 over overflow-hidden`} src={img1} alt="" />
+        <div className="relative h-screen flex items-center justify-items-between">
+            <div className="absolute left-[-40rem] ">
+            <img className={`object-cover w-full h-3/4 over overflow-hidden`} src={img1} alt="" />
+            </div>
              <div className="absolute right-10 w-1/3 z-10 flex flex-col items-center ">
                 <h1 className="text-4xl font-bebas text-center">THE SUPER SPORTS CAR OF THE FUTURE</h1>
                 <p className="text-md font-roboto text-justify pt-2 w-2/3">Automobili Lamborghini looks to the future with a visionary approach, carrying our core values to extremes. Dictating the requirements of the third millennium, the Terzo Millennio combines energy efficiency and innovative materials to forge a path in the electric super sports car segment, guaranteeing the purest driving experience.</p>
